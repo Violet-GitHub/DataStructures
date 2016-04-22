4944fed4f92d0dcd5d319b63bbece397f0bb8ba1
Violet-GitHub <c20121101@sina.com>
2016-04-22 16:56:46 +0800

    TestData3

--- TestData3.java
+++ TestData3.java
@@ -1,31 +1,84 @@
 package com.example.demo;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Random;
+
 public class TestData3 {
-	public static int[] datas={4,56,89,2,3,0,9,6,8};
-	public static int i=0,j=0;
+	public static int[] myDatas;
+	public static Random random=new Random();
+	public static int myN=0;
+	
 	public static void main(String[] args) {
-		for (; i < datas.length; i++) {
-			sort(datas[i]);
-		}
-		pirnt(datas);
+		SimpleDateFormat sdf=new SimpleDateFormat("yyyy-MM-dd HH:mm:ss.SSS");
+		myN=5000;
+		myDatas=new int[myN];
+		initDatas(myDatas,myN);
+		
+		System.out.print(sdf.format(new Date())+"\n");
+		sort2(myDatas,0,myDatas.length-1);
+		System.out.print(sdf.format(new Date())+"\n");
+		pirnt(myDatas);
+	
+		System.out.print(sdf.format(System.currentTimeMillis())+"\n");
+		sort1(myDatas);
+		System.out.print(sdf.format(new Date())+"\n");
+		pirnt(myDatas);
 	}
 	
-	public static void sort(int data){
-		for (int k = 0; k < datas.length; k++) {
-			if(datas[k]>data){
-				j=data;
-				data=datas[k];
-				datas[k]=data;
+	public static void initDatas(int[] datas,int n){
+		for (int i=0;i<n;i++) {
+			datas[i]=random.nextInt(n);
+		}
+	}
+	
+	public static void sort2(int[] myDatas,int s,int e){
+		if(s<e){
+			int left=s;//下次递归的最小坐标值；
+			int right=e;//下次递归的最大坐标值
+			//取一个参考值；
+			int provit=myDatas[s];
+			//把myDatas分开成左右两个部分；
+			while(left<right){
+				//在右边找出小于provit的值，并放到provit的左边
+				while(left<right&&myDatas[right]>provit){
+					right--;
+				}
+				myDatas[left]=myDatas[right];
+				//在左边找出一个大于provit的值，并放到provit的右边
+				while(left<right&&myDatas[left]<=provit){
+					left++;
+				}
+				myDatas[right]=myDatas[left];
 			}
-			//sort(datas[k++]);
+			//把参考值放在坐标left=right的坐标点上
+			myDatas[left]=provit;//此时坐标值left=right
+			//递归排序provit左边数；
+			sort2(myDatas,s,left-1);
+			//递归排序provit右边数；
+			sort2(myDatas,left+1,e);
 		}
 		
+	}
+	
+	public static void sort1(int[] datas){
+		int j=0;
+		for (int i=0; i < datas.length; i++) {
+			for (int k = 0; k < datas.length; k++) {
+				if(datas[i]>datas[k]){
+					j=datas[k];
+					datas[k]=datas[i];
+					datas[i]=j;
+				}
+			}
+		}
 	}
 	
 	public static void pirnt(int[] in){
 		for (int i : in) {
 			System.out.print(i+",");
 		}
+		System.out.println("\n");
 	}
 	
 }
