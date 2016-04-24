2c498733f5d60feb4f93f0404a91a47793b2a11c
Violet-GitHub <c20121101@sina.com>
2016-04-25 00:21:31 +0800

    快速、冒泡、插入、选择排序

--- TestData3.java
+++ TestData3.java
@@ -1,4 +1,4 @@
-package com.example.demo;
+package com.example;
 
 import java.text.SimpleDateFormat;
 import java.util.Date;
@@ -14,57 +14,101 @@ public class TestData3 {
 		myN=5000;
 		myDatas=new int[myN];
 		initDatas(myDatas,myN);
+
+		System.out.print(sdf.format(new Date())+"\n");
+		sort4(myDatas);
+		System.out.print(sdf.format(new Date())+"\n");
+		pirnt(myDatas, "冒泡排序");
+
+		System.out.print(sdf.format(new Date())+"\n");
+		sort3(myDatas);
+		System.out.print(sdf.format(new Date())+"\n");
+		pirnt(myDatas, "插入排序");
 		
 		System.out.print(sdf.format(new Date())+"\n");
 		sort2(myDatas,0,myDatas.length-1);
 		System.out.print(sdf.format(new Date())+"\n");
-		pirnt(myDatas);
+		pirnt(myDatas,"快速排序");
 	
 		System.out.print(sdf.format(System.currentTimeMillis())+"\n");
 		sort1(myDatas);
 		System.out.print(sdf.format(new Date())+"\n");
-		pirnt(myDatas);
+		pirnt(myDatas,"选择排序");
 	}
-	
-	public static void initDatas(int[] datas,int n){
-		for (int i=0;i<n;i++) {
-			datas[i]=random.nextInt(n);
+
+	/**
+	 * 冒泡排序
+	 * @param datas
+	 */
+	public static void sort4(int[] datas){
+		int temp;
+		for(int i=0;i<datas.length;i++){
+			for(int j=i+1;j<datas.length;j++){
+				if(datas[i]>datas[j]){
+					temp=datas[i];
+					datas[i]=datas[j];
+					datas[j]=temp;
+				}
+			}
 		}
 	}
-	
-	public static void sort2(int[] myDatas,int s,int e){
+
+	/**
+	 * 插入排序  工作原理:通过构建有序序列，对于未排序数据，在已排序序列中从后向前扫描，找到相应位置并插入。
+	 * @param datas
+	 */
+	public static void sort3(int[] datas){
+		int temp;
+		for(int i=1;i<datas.length;i++){
+			for (int j=0;j<i;j++){
+				if(datas[i]<datas[j]){
+					temp=datas[i];
+					//把datas[i]放在datas[j]所在位置,所以把i到j之间到数往后挪一位
+					for(int k=i;k>=j;k--){
+						if (k<1)break;
+						datas[i]=datas[i-1];
+					}
+					datas[j]=temp;
+				}
+			}
+		}
+	}
+
+	//快速排序
+	public static void sort2(int[] datas,int s,int e){
 		if(s<e){
 			int left=s;//下次递归的最小坐标值；
 			int right=e;//下次递归的最大坐标值
 			//取一个参考值；
-			int provit=myDatas[s];
+			int provit=datas[s];
 			//把myDatas分开成左右两个部分；
 			while(left<right){
 				//在右边找出小于provit的值，并放到provit的左边
-				while(left<right&&myDatas[right]>provit){
+				while(left<right&&datas[right]>provit){
 					right--;
 				}
-				myDatas[left]=myDatas[right];
+				datas[left]=datas[right];
 				//在左边找出一个大于provit的值，并放到provit的右边
-				while(left<right&&myDatas[left]<=provit){
+				while(left<right&&datas[left]<=provit){
 					left++;
 				}
-				myDatas[right]=myDatas[left];
+				datas[right]=datas[left];
 			}
 			//把参考值放在坐标left=right的坐标点上
-			myDatas[left]=provit;//此时坐标值left=right
+			datas[left]=provit;//此时坐标值left=right
 			//递归排序provit左边数；
-			sort2(myDatas,s,left-1);
+			sort2(datas,s,left-1);
 			//递归排序provit右边数；
-			sort2(myDatas,left+1,e);
+			sort2(datas,left+1,e);
 		}
 		
 	}
-	
+
+	//选择排序 工作原理:首先在未排序序列中找到最小元素，存放到排序序列的起始位置，然后，再从剩余未排序元素中继续寻找最小元素，然后放到排序序列末尾。以此类推，直到所有元素均排序完毕
 	public static void sort1(int[] datas){
 		int j=0;
 		for (int i=0; i < datas.length; i++) {
-			for (int k = 0; k < datas.length; k++) {
+			for (int k = i; k < datas.length; k++) {
 				if(datas[i]>datas[k]){
 					j=datas[k];
 					datas[k]=datas[i];
@@ -73,8 +117,15 @@ public class TestData3 {
 			}
 		}
 	}
+
+	public static void initDatas(int[] datas,int n){
+		for (int i=0;i<n;i++) {
+			datas[i]=random.nextInt(n);
+		}
+	}
 	
-	public static void pirnt(int[] in){
+	public static void pirnt(int[] in ,String str){
+		System.out.print(str + ":\n");
 		for (int i : in) {
 			System.out.print(i+",");
 		}
